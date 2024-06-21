bd5bdd072c2f3d42c14a2da3237fb0fec775ba1c
Cauã Cristian Inocêncio <125278407+CauaCristian@users.noreply.github.com>
2024-06-21 13:58:56 -0300

    add

---
+++ 05_curso_spring/api_spring/src/main/java/com/caua/api_spring/ApiSpringApplication.java
@@ -0,0 +1,15 @@
+package com.caua.api_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiSpringApplication.class, args);
+		System.out.println("hello-world");
+	}
+
+}
